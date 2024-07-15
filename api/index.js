c1119390d828833cfa9b8b8c764f6e7e16d1abab
cristonpinto <cristonhimasha73@gmail.com>
2024-07-15 23:34:16 +0530

    create a test API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'; // using express we can work with back end and the APIs
 import mongoose from 'mongoose'; // using mongoose we can work with the database
 import dotenv from 'dotenv'; // using dotenv we can work with the environment variables
+import userRouter from './routes/user.route.js'; // using userRouter we can work with the user routes
 
 dotenv.config(); // Load environment variables from .env file
 
@@ -8,12 +9,17 @@ const app = express();
 const PORT = process.env.PORT || 3000; // Use PORT from environment variables or default to 3000
 
 // MongoDB connection string from environment variables
-const mongoURI = process.env.MONGODB_URI ;
+const mongoURI = process.env.MONGODB_URI;
+
+if (!mongoURI) {
+  console.error('Error: MONGODB_URI is not defined in environment variables');
+  process.exit(1);
+}
 
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
-    
+
     // Start the server only after successful connection to MongoDB
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
@@ -21,8 +27,11 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   })
   .catch(err => {
     console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
   });
 
-app.get('/', (req, res) => {
+app.use('/api/user', userRouter);
+
+app.get('/test', (req, res) => {
   res.send('Hello World!');
 });
